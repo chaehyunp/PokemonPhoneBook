9e0e4e2b382a15ce4a496d380edc75f76b2455cd
chhue <chhue96@gmail.com>
2024-12-09 23:02:15 +0900

    init: 프로젝트 생성
    - 코드베이스 UI로 사용하기 위해 스토리보드 제거
    - SnapKit 추가

---
+++ PokemonPhoneBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonPhoneBook
+//
+//  Created by CHYUN on 12/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
